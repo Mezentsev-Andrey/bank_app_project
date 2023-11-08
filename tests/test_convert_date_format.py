0b4b69eb5d07c4995f5fd5e5919781488171df57
Andrey Mezentsev <mezentsew@mail.ru>
2023-11-08 18:01:53 +0300

    Add docstrings to five test modules in package tests.

--- tests/test_convert_date_format.py
+++ tests/test_convert_date_format.py
@@ -2,4 +2,8 @@ from src.widget import convert_date_format
 
 
 def test_convert_date_format() -> None:
+    """
+    Тестовая функция проверяющая правильность конвертируемости формата даты.
+    :return: None
+    """
     assert convert_date_format("2019-07-11T02:26:18.671407") == "11.07.2019"
--- tests/test_list_dictionaries.py
+++ tests/test_list_dictionaries.py
@@ -4,7 +4,11 @@ from src.processing import filter_by_state, sort_dict_list
 
 
 @pytest.fixture
-def collection_dictionaries_list():
+def collection_dictionaries_list() -> list[dict]:
+    """
+    Фикстура создающая и возвращающая список словарей, представляющих собой коллекцию данных о транзакциях
+    :return: список словарей, представляющих коллекцию данных о транзакциях
+    """
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -14,6 +18,12 @@ def collection_dictionaries_list():
 
 
 def test_filter_by_state(collection_dictionaries_list: list[dict]) -> None:
+    """
+    Тестовая функция принимающая список словарей и фильтруеющая его, оставляя только те элементы,
+    у которых значение ключа 'state' соответствует заданному состоянию
+    :param collection_dictionaries_list: cписок словарей, представляющих коллекцию данных о транзакциях
+    :return: None
+    """
     assert filter_by_state(collection_dictionaries_list, state="EXECUTED") == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -21,6 +31,11 @@ def test_filter_by_state(collection_dictionaries_list: list[dict]) -> None:
 
 
 def test_sort_dict_list(collection_dictionaries_list: list[dict]) -> None:
+    """
+    Тестовая функция принимающая список словарей и сортирующая их по ключу 'date' в заданном порядке
+    :param collection_dictionaries_list: cписок словарей, представляющих коллекцию данных о транзакциях
+    :return: None
+    """
     assert sort_dict_list(collection_dictionaries_list) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
